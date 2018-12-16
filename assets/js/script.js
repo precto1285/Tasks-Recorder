5e3dbde383ef7b993a06f43724e5c29112f9cf64
Philippe Recto <precto1285@gmal.com>
2018-12-16 14:39:40 -0800

    reset task app

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,6 @@
 // UI Variables
 const form = document.querySelector('#taskInput');
-const taskList = document.querySelector('.taskList')
+const taskList = document.querySelector('.taskList');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector();
+const taskInput = document.querySelector('#task');
